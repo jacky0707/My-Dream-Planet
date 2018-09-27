1bf15e57c49bcec7e702d02191c1a825fbad79a2
songjie <songjie1120@g.ncu.edu.tw>
2018-09-27 18:32:15 +0800

    next too much

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -22,80 +22,127 @@ function initDreams(){
         let dreamElement = document.getElementById('button-part').children[dream]
         let handler = function(event){
             //按鈕座標
-            buttonX = getDreamPosition(event.target).x
-            buttonY = getDreamPosition(event.target).y
             
-            //動畫主角
-            let flyingDream = document.createElement("DIV")
-            flyingDream.classList.add('adding-dream')
-            flyingDream.setAttribute("style","position:absolute;left:"+buttonX+"px;top:"+buttonY+"px;background:"+getComputedStyle(event.target).backgroundImage+"no-repeat scroll 50% 50% / contain padding-box border-box")
-            document.body.appendChild(flyingDream)
-
-            //飛行終點
-            flyingDream.style.left = "calc("+getPlanetCenter().x+"px - 3vw)"
-            flyingDream.style.top = "calc("+getPlanetCenter().y+"px - 3vw)"
-            flyingDream.style.transform="scale(2)"
-            flyingDream.addEventListener('transitionend',function(){
-                flyingDream.style.transform="scale(0)"
-                setTimeout(function(){flyingDream.remove()},2000)
-            })
             switch(event.target.id) {
                 case "house":
+                if(document.getElementsByClassName('planet-house').length==9){
+                    alert('夢想星的住宅用地已經用完了><')
+                }else{
+                    flyingAnimation(event)
                     let house = document.createElement("DIV")
+                    house.setAttribute("id",event.target.id+document.getElementsByClassName('planet-house').length)
+                    console.log(event.target.id+document.getElementsByClassName('planet-house').length)
                     house.classList.add('planet-house')
                     house.style.transform="rotate( "+(Math.floor((Math.random() * 9) + 1))*40+"deg )"
                     document.getElementById('planet').appendChild(house)
+                    addToList("house",document.getElementsByClassName('planet-house').length-1)
                     setTimeout(function(){house.style.display="block"},2000)
-                    addToList("house")
+                    
+                }
                     break;
                 case "car":
+                if(document.getElementsByClassName('planet-car').length==31){
+                    alert('夢想星只能負載30台車，還請您委屈一下。')
+                }else{
+                    flyingAnimation(event)
                     let car = document.createElement("DIV")
+                    car.setAttribute("id",event.target.id+document.getElementsByClassName('planet-car').length)
                     car.classList.add('planet-car')
                     car.style.transform="rotate( 0deg )"
                     document.getElementById('planet').appendChild(car)
+                    addToList("car",document.getElementsByClassName('planet-car').length-1)
                     setTimeout(function(){car.style.display="block"},2000)
-                    break;
+                    
+                }
+                break;
                 case "wedding":
+                if(document.getElementsByClassName('planet-ring').length==1){
+                    alert('夢想星祝福一次就找到幸福。')
+                }else{
+                    flyingAnimation(event)
                     let wedding = document.createElement("DIV")
+                    wedding.setAttribute("id",event.target.id+document.getElementsByClassName('planet-ring').length)
                     wedding.classList.add('planet-ring')
                     wedding.style.transform="rotateX(-45deg)"
                     document.getElementById('planet').appendChild(wedding)
+                    addToList("wedding",document.getElementsByClassName('planet-ring').length-1)
                     setTimeout(function(){wedding.style.display="block"},2000)
-                    break;
+                    
+                }
+                break;
                 case "kid":
+                if(document.getElementsByClassName('planet-kid').length==50){
+                    alert('一個不嫌少兩個恰恰好，50個只怕肚皮吃不消')
+                }else{
+                    flyingAnimation(event)
                     let kid = document.createElement("DIV")
+                    kid.setAttribute("id",event.target.id+document.getElementsByClassName('planet-kid').length)
                     kid.classList.add('planet-kid')
                     kid.style.transform="rotate( "+Math.floor((Math.random() * 360) + 1)+"deg )"
                     document.getElementById('planet').appendChild(kid)
+                    addToList("kid",document.getElementsByClassName('planet-kid').length-1)
                     setTimeout(function(){kid.style.display="block"},2000)
-                    break;
+                }
+                break;
                 case "parent":
+                if(document.getElementsByClassName('planet-parent').length==1){
+                    alert('長輩數量已達夢想星上限')
+                }else{
+                    flyingAnimation(event)
                     let parent = document.createElement("DIV")
+                    parent.setAttribute("id",event.target.id+document.getElementsByClassName('planet-parent').length)
                     parent.classList.add('planet-parent')
                     parent.style.transform="rotate(-135deg) rotateX(-30deg)"
                     document.getElementById('planet').appendChild(parent)
+                    addToList("parent",document.getElementsByClassName('planet-parent').length-1)
                     setTimeout(function(){parent.style.display="block"},2000)
-                    break;
+                }        
+                break;
                 case "travel":
+                if(document.getElementsByClassName('planet-plane').length==31){
+                    alert('太多飛機夢想星受不了的。')
+                }else{
+                    flyingAnimation(event)
                     let plane = document.createElement("DIV")
+                    plane.setAttribute("id",event.target.id+document.getElementsByClassName('planet-plane').length)
                     plane.classList.add('planet-plane')
                     plane.style.transform="rotate( 0deg )"//" + (Math.floor((Math.random() * 360) + 1)) + "
                     document.getElementById('planet').appendChild(plane)
+                    addToList("travel",document.getElementsByClassName('planet-plane').length-1)
                     setTimeout(function(){plane.style.display="block"},2000)
-                    break;
+                }
+                break;
                 case "retire":
+                if(document.getElementsByClassName('planet-retire').length==1){
+                    alert('夢想星上退休一次就可以享天倫之樂了呦')
+                }else{
+                    flyingAnimation(event)
                     let retire = document.createElement("DIV")
+                    retire.setAttribute("id",event.target.id+document.getElementsByClassName('planet-retire').length)
                     retire.classList.add('planet-retire')
                     document.getElementById('planet').appendChild(retire)
+                    addToList("retire",document.getElementsByClassName('planet-retire').length-1)
                     setTimeout(function(){retire.style.display="block"},2000)
-                    break;
+                }
+                break;
                 case "entrepreneur":
+                if(document.getElementsByClassName('planet-entrepreneur').length==9){
+                    alert('9命怪貓,9次創業')
+                    createElement("DIV",{atrs:{
+                        innerHTML:'9命怪貓,9次創業',
+                        className:'too-much'
+                    }},document.getElementsByTagName("BODY")[0])
+                }else{
+                    flyingAnimation(event)
                     let entrepreneur = document.createElement("DIV")
+                    entrepreneur.setAttribute("id",event.target.id+document.getElementsByClassName('planet-entrepreneur').length)
                     entrepreneur.classList.add('planet-entrepreneur')
                     entrepreneur.style.transform="rotate( "+((Math.floor((Math.random() * 9) + 1))*40-20)+"deg )"
                     document.getElementById('planet').appendChild(entrepreneur)
+                    addToList("entrepreneur",document.getElementsByClassName('planet-entrepreneur').length-1)
                     setTimeout(function(){entrepreneur.style.display="block"},2000)
-                    break;
+                }
+                break;
                 case "others":
                     break;
             }
@@ -104,6 +151,26 @@ function initDreams(){
     }
 }
 
+function flyingAnimation(event){
+    //按鈕座標
+    buttonX = getDreamPosition(event.target).x
+    buttonY = getDreamPosition(event.target).y
+    
+    //動畫主角
+    let flyingDream = document.createElement("DIV")
+    flyingDream.classList.add('adding-dream')
+    flyingDream.setAttribute("style","position:absolute;left:"+buttonX+"px;top:"+buttonY+"px;background:"+getComputedStyle(event.target).backgroundImage+"no-repeat scroll 50% 50% / contain padding-box border-box")
+    document.body.appendChild(flyingDream)
+
+    //飛行終點
+    flyingDream.style.left = "calc("+getPlanetCenter().x+"px - 3vw)"
+    flyingDream.style.top = "calc("+getPlanetCenter().y+"px - 3vw)"
+    flyingDream.style.transform="scale(2)"
+    flyingDream.addEventListener('transitionend',function(){
+        flyingDream.style.transform="scale(0)"
+        setTimeout(function(){flyingDream.remove()},2000)
+    })
+}
 function getDreamPosition(button){
     let buttonSquare = button.getBoundingClientRect()
     let buttonPosition = {}
@@ -126,51 +193,59 @@ function getPlanetCenter(){
     return planetPosition
 }
 
-function addToList(type){
-    let newList = createList("DIV",{atrs:{
+function addToList(type,dreamLength){
+    let listId = type+dreamLength
+    let newList = createElement("DIV",{atrs:{
+        id:"list"+listId,
         className:"list-label"
     }},document.getElementById('dream-list-content'))
 
-    let dreamName = createList("INPUT",{atrs:{
+    let dreamName = createElement("INPUT",{atrs:{
         className:"dream-name",
         placeholder:type,
         type:"text"
     }},newList)
 
-    let listSelect = createList("SELECT",{atrs:{
+    let listSelect = createElement("SELECT",{atrs:{
         className:"list-select",
         type:"text"
     }},newList)
 
-    let opt1 = createList("OPTION",{atrs:{
+    let opt1 = createElement("OPTION",{atrs:{
         innerHTML:"每年",
         value:"每年"
     }},listSelect)
-    let opt2 = createList("OPTION",{atrs:{
+    let opt2 = createElement("OPTION",{atrs:{
         innerHTML:"每月",
         value:"每月"
     }},listSelect)
-    let opt3 = createList("OPTION",{atrs:{
+    let opt3 = createElement("OPTION",{atrs:{
         innerHTML:"一次",
         value:"一次"
     }},listSelect)
 
-    let dreamCost = createList("INPUT",{atrs:{
+    let dreamCost = createElement("INPUT",{atrs:{
         className:"dream-money dream-name",
         value:"50000",
         type:"text",
     }},newList)
     dreamCost.onkeyup=function(){this.value=this.value.replace(/\D/g,'')}
     dream.onafterpaste=function(){this.value=this.value.replace(/\D/g,'')}
-    
-    let dollar = createList("DIV",{atrs:{
+
+    let dollar = createElement("DIV",{atrs:{
         innerHTML:"元"
     }},newList)
-    let cancelButton = createList("BUTTON",{atrs:{
-        className:"list-cancel"
+    let cancelButton = createElement("BUTTON",{atrs:{
+        className:"list-cancel",
     }},newList)
+    cancelButton.onclick=function(){
+        id = this.parentElement.id
+        listId = id.split("list")[1]
+        document.getElementById(id).remove()
+        document.getElementById(listId).remove()
+    }
 }
-function createList(tagName,settings,parentElement){
+function createElement(tagName,settings,parentElement){
     let obj=document.createElement(tagName);
 	if(settings.atrs){setAttributes(obj,settings.atrs);}
 	if(settings.stys){setStyles(obj,settings.stys);}
